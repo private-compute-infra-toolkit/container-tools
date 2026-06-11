52440e343a9c7f9888dd7ddfeb188127960e8de5
PCIT Release Pipeline <private-compute-infra-toolkit-devkit-copybara@google.com>
2026-06-11 14:54:32 +0000

    Release version 0.4.0
    GitOrigin-RevId: 73dc44c4d4c85edeca53de1456ec02f95e946eb4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file. See [commit-and-tag-version](https://github.com/absolute-version/commit-and-tag-version) for commit guidelines.
 
+## 0.4.0 (2026-06-11)
+
+
+### Features
+
+* Defer CloudWatch Agent configuration to boot time
+
 ## 0.3.0 (2026-06-10)
 
 
--- build_defs/aws/enclave/setup_enclave.sh
+++ build_defs/aws/enclave/setup_enclave.sh
@@ -48,16 +48,7 @@ sudo systemctl enable configure-fluent-bit.service
 
 sudo yum install amazon-cloudwatch-agent -y
 
-# Configure Amazon CloudWatch Agent if config is present (pre-configure at build time)
-if [ -f /opt/google/metrics/amazon-cloudwatch-agent.json ] && [ -f /opt/google/metrics/prometheus.yaml ]; then
-  sudo cp /opt/google/metrics/prometheus.yaml /opt/aws/amazon-cloudwatch-agent/etc/prometheus.yaml
-  sudo cp /opt/google/metrics/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-      -a fetch-config \
-      -m ec2 \
-      -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-  sudo systemctl enable amazon-cloudwatch-agent
-fi
+sudo systemctl enable metrics-exporter.service
 
 sudo systemctl enable vsockproxy
 
